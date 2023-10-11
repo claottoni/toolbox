aa4a678f1e7a49ffedf38178ea89443adda185d1
Claudio Ottoni <ottocla@gmail.com>
2023-10-11 16:54:46 +0200

    update

---
+++ amr_blastn_parser_v2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# this script strips out the info from a flagstat in one text file (redirected from command)
+# Ex: flagstasts.py ~/Desktop/*.txt > results.txt
+
+import re
+import sys
+import os
+
+FileList = sys.argv[1:]
+Header = 0
+for InfileName in FileList:
+  if Header == 0:
+    Header = "Sample\tHits\tTotalReads\tcpmNormalizedReads" 
+    print (Header)
+    Infile = open(InfileName, 'r')
+    ReadsName = []
+    for line in Infile:
+      line = line.strip('\n')
+      element = line.split('\t')
+      ReadsName.append(element[0])
+    Hits = len(sorted(set(ReadsName)))
+    FastaName = InfileName.replace("aro.blastn.out", "fasta")
+    FastaFile = open(FastaName, 'r')
+    TotalReads = len([1 for line in open(FastaName) if line.startswith(">")])
+    NormalizedReads = Hits / TotalReads * 1000000
+    print ("%s\t%s\t%s\t%f" % (os.path.basename(InfileName), Hits, TotalReads, NormalizedReads))
+  else:
+    Infile=open(InfileName, 'r')
+    ReadsName = []
+    for line in Infile:
+      line = line.strip('\n')
+      element = line.split('\t')
+      ReadsName.append(element[0])
+    Hits = len(sorted(set(ReadsName)))
+    FastaName = InfileName.replace("aro.blastn.out", "fasta")
+    FastaFile = open(FastaName, 'r')
+    TotalReads = len([1 for line in open(FastaName) if line.startswith(">")])	
+    NormalizedReads = Hits / TotalReads * 1000000
+    print ("%s\t%s\t%s\t%f" % (os.path.basename(InfileName), Hits, TotalReads, NormalizedReads))
